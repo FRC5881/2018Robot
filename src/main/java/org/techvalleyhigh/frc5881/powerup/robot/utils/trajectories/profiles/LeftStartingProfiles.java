c0f10814c66a3a732538f325f22fe6114ef7927b
jhausgaard <jhausgaard@techvalleyhigh.org>
2018-02-07 13:59:12 -0500

    Got rid of useless import

--- src/main/java/org/techvalleyhigh/frc5881/powerup/robot/utils/trajectories/profiles/LeftStartingProfiles.java
+++ src/main/java/org/techvalleyhigh/frc5881/powerup/robot/utils/trajectories/profiles/LeftStartingProfiles.java
@@ -5,8 +5,6 @@ import org.techvalleyhigh.frc5881.powerup.robot.utils.trajectories.Autonomous;
 import org.techvalleyhigh.frc5881.powerup.robot.utils.trajectories.ProfileGroup;
 import org.techvalleyhigh.frc5881.powerup.robot.utils.trajectories.TrajectoryUtil;
 
-import java.util.HashMap;
-
 import static org.techvalleyhigh.frc5881.powerup.robot.utils.trajectories.TrajectoryUtil.AutoTarget.*;
 import static org.techvalleyhigh.frc5881.powerup.robot.utils.trajectories.TrajectoryUtil.defaultConfig;
 
